b306ed50e77d3c9652ba94da3e93c4eefb3b835a
Rahatul99 <rahatul99@gmail.com>
2024-08-26 14:26:06 +0600

    parsing request

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 //dependencies
 const http = require("http");
+const url = require("url");
+const { StringDecoder } = require("string_decoder"); //stringDecoder is a class
 
 //app object - module scaffolding
 const app = {};
@@ -19,7 +21,29 @@ app.createServer = () => {
 
 //handle request and response
 app.handleReqRes = (req, res) => {
-  res.end("Hello world");
+  //request handling
+  //get the url and parse it
+  const parsedUrl = url.parse(req.url, true);
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/*|\/*$/g, "");
+  const method = req.method.toLowerCase();
+  const queryStringObject = parsedUrl.query;
+  const headersObject = req.headers;
+
+  const decoder = new StringDecoder("utf-8");
+  let realData = "";
+
+  req.on("data", (buffer) => {
+    realData += decoder.write(buffer);
+  });
+
+  req.on("end", () => {
+    realData += decoder.end();
+    console.log(realData);
+
+    //response handle
+    res.end("Hello world hi");
+  });
 };
 
 //start the server
